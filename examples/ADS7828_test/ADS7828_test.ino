f19eaf87443d9be285e4258fb14d3c06339e30c7
Ashok Singhal <wuffusg@gmail.com>
2023-02-20 07:34:59 -0800

    Create ADS7828_test.ino

---
+++ examples/ADS7828_test/ADS7828_test.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <ADS7828.h>
+
+ADS7828 adc;
+
+void setup()
+{
+  // Initialize I2C communication as MASTER
+  Wire.begin();
+  Wire.setClock(400000L);
+  adc.begin(0);
+  Serial.begin(115200);
+  delay(300);
+}
+
+void loop()
+{
+    int adcData[8];
+    for (int channel = 0; channel < 8; channel++) {
+      adcData[channel] = adc.read(channel);
+      Serial.print(adcData[channel]); Serial.print(" ");
+    }
+    Serial.println();
+}
