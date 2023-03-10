7ce2d2535e1e354fc169c75256b7bff1acd6ced5
Ashok Singhal <eebothobby@gmail.com>
2023-03-10 13:48:30 -0800

    Update ADS7828_test.ino
    Add comments to show how to use a different I2C bus than the default Wire instance

--- examples/ADS7828_test/ADS7828_test.ino
+++ examples/ADS7828_test/ADS7828_test.ino
@@ -1,5 +1,9 @@
 #include <ADS7828.h>
 
+// If using an I2C bus that is different from default Wire,
+// for example SDA2 = PB3, SCL2 = PB10 on STM32F411-based "black pill"
+// TwoWire Wire2(PB3, PB10);
+
 ADS7828 adc;
 
 void setup()
@@ -7,7 +11,16 @@ void setup()
   // Initialize I2C communication as MASTER
   Wire.begin();
   Wire.setClock(400000L);
+  
+  // If using Wire2
+  // Wire2.begin();
+  // Wire2.setClock(400000L);
+  
   adc.begin(0);
+  
+  // If using Wire2
+  // adc.begin(0, &Wire2);
+
   Serial.begin(115200);
   delay(300);
 }
